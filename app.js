9efc0af1bc39cf6e9b8ec415cb9c27a79938774b
musicstudio <musicstudio@musics-MBP.home>
2020-07-08 21:01:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Scenario 1
+// let str1 = 'Coding !!! is !fun!';
+// console.log(str1);
+// let arr1 = str1.split("!");
+// console.log(str1);
+// console.log(arr1);
+// let str2 = arr1.join();
+// console.log(arr1);
+// console.log(str2);
+
+
+// Scenario 2
+// let str1 = 'Coding !!! is !fun!';
+// console.log(str1);
+// let arr1 = str1.split("!");
+// console.log(str1 + "     split does not change original");
+// console.log(arr1);
+// let str2 = arr1.join("");
+// console.log(arr1 + "     join does not change original");
+// console.log(str2);
+
+// Scenario 3 
+// let str = 'Coding is fun!';
+// let rev=str.split("").reverse().join("");
+// console.log(str);
+// console.log(rev);
+
+
+// Scenario 4
+// let arrNum = [ 0, 1, 2, 3 ];
+// let str = arrNum.join();
+// console.log(str);
+
+// Scenario 5
+// let arrNum = [ 0, 1, 2, 3 ];
+// let str = arrNum.join("");
+// console.log(str);
